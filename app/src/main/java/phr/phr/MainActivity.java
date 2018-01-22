fdc933735f2f8b5ae4388d8b7038160d58dde022
Anupam Sharma <sharm12f@uwindsor.ca>
2018-01-21 20:46:49 -0500

    This commit might as well be useless but a back up none the less

--- app/src/main/java/phr/phr/MainActivity.java
+++ app/src/main/java/phr/phr/MainActivity.java
@@ -2,12 +2,30 @@ package phr.phr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import phr.lib.Lib;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button login_button = findViewById(R.id.login_button);
+        final Button reser_password_button = findViewById(R.id.reset_password_button);
+        final Button password_button = findViewById(R.id.reset_password_button);
+        final EditText email_input = findViewById(R.id.email_input);
+        final EditText password_input=  findViewById(R.id.password_input);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                System.out.println(Lib.login(email_input.getText().toString(), password_input.getText().toString()));
+            }
+        });
     }
 }
