900fa1b5937ba0b80ba202a14f774e8d16b2fd94
Anupam Sharma <sharm12f@uwindsor.ca>
2018-04-10 22:12:46 -0400

    removed the perset login from the login button, reset pass still wont work, but will log you in as a default HP

--- app/src/main/java/phr/phr/LogIn.java
+++ app/src/main/java/phr/phr/LogIn.java
@@ -59,10 +59,10 @@ public class LogIn extends AppCompatActivity {
                             p.show();
                         }
                         protected Void doInBackground(Void... progress) {
-                            //if(email_input.getText().toString().equals("") || password_input.getText().toString().equals(""))
-                            //    return null;
-                            //user = Lib.login(email_input.getText().toString(), password_input.getText().toString());
-                            user = Lib.login("app@app.com", "p");
+                            if(email_input.getText().toString().equals("") || password_input.getText().toString().equals(""))
+                                return null;
+                            user = Lib.login(email_input.getText().toString(), password_input.getText().toString());
+                            //user = Lib.login("app@app.com", "p");
                             return null;
                         }
                         protected void onPostExecute(Void Void){
